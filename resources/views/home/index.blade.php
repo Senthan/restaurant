55b1e1c4c3d3985a46bc51a2a1155ae56420aad9
Senthan <senthaneng@gmail.com>
2017-06-10 02:29:25 +0800

    Update index.blade.php
    Update Style

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -14,19 +14,6 @@
         margin: 0;
         padding: 0;
       }
-      #map {
-        height: 100%;
-        float: left;
-        width: 70%;
-        height: 100%;
-      }
-      #directions-panel {
-        margin-top: 10px;
-        background-color: #FFEE77;
-        padding: 10px;
-        overflow: scroll;
-        height: 174px;
-      }
     </style>
   </head>
   <body>
@@ -53,21 +40,30 @@
 		<div class="ui text container"> 
 			<h3 class="ui header centered"> Restaurant Direction</h3>
 		</div>
-		<div class="ui grid">
-		  <div class="ten wide column">
+		<div class="row">
+		  <div class="col-md-8">
 			<div class="ui embed" id="map" ></div>
 		  </div>
-		  <div class="six wide column">
-			<b>Select of Travel Restaurant: </b>
-			<select id="restaurant">
-			<option value="null">Select Restaurant</option>
-			@foreach($restaurants as $restaurant)
-			  <option value="{{ $restaurant }}">{{ $restaurant->name }}</option>
-			@endforeach 
-			</select>
-			<br>
-			<input type="submit" id="submit">
-			<div id="directions-panel"></div>
+		  <div class="col-md-4">
+			<form class="ui form">
+				  <h4 class="ui dividing header">Shipping Information</h4>
+				  <div class="field">
+					<label>Select of Travel Restaurant</label>
+					  <div class="field">
+							<select id="restaurant" class="ui right attached dropdown">
+							<option value="null">Select Restaurant</option>
+							@foreach($restaurants as $restaurant)
+							  <option value="{{ $restaurant }}">{{ $restaurant->name }}</option>
+							@endforeach 
+						</select>
+					  </div>
+				  </div>
+			  <div class="ui button green tiny" tabindex="0" type="submit" id="submit">Get Direction</div>
+			  
+
+			  
+			  <div id="directions-panel" class="ui info message hidden"></div>
+			</form>	
 		  </div>
 		</div>
 	  </div>
@@ -75,34 +71,35 @@
     <script>
       function initMap() {
 	 
-			var locations = {!! $restaurants !!};
-			var map = new google.maps.Map(document.getElementById('map2'), {
-               zoom: 10,
-               center: new google.maps.LatLng(1.28, 103.8),
-               mapTypeId: google.maps.MapTypeId.ROADMAP
-             });
-             var infowindow = new google.maps.InfoWindow();
-             var marker, i;
-             for (i = 0; i < locations.length; i++) {  
-               marker = new google.maps.Marker({
-                 position: new google.maps.LatLng(locations[i]['latitude'], locations[i]['longitude']),
-                 map: map
-               });
-               google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                 return function() {
-                   infowindow.setContent(locations[i]['name']);
-                   infowindow.open(map, marker);
-                 }
-               })(marker, i));
-             }
+		var locations = {!! $restaurants !!};
+		var map = new google.maps.Map(document.getElementById('map2'), {
+		   zoom: 10,
+		   center: new google.maps.LatLng(1.28, 103.8),
+		   mapTypeId: google.maps.MapTypeId.ROADMAP
+		 });
+		 var infowindow = new google.maps.InfoWindow();
+		 var marker, i;
+		 for (i = 0; i < locations.length; i++) {  
+		   marker = new google.maps.Marker({
+			 position: new google.maps.LatLng(locations[i]['latitude'], locations[i]['longitude']),
+			 map: map
+		   });
+		   google.maps.event.addListener(marker, 'click', (function(marker, i) {
+			 return function() {
+			   infowindow.setContent(locations[i]['name']);
+			   infowindow.open(map, marker);
+			 }
+		   })(marker, i));
+		 }
  
  
         var directionsService = new google.maps.DirectionsService;
         var directionsDisplay = new google.maps.DirectionsRenderer;
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 6,
-          center: {lat: 41.85, lng: -87.65}
+          center: {lat: 1.27, lng: 104.8}
         });
+		
         directionsDisplay.setMap(map);
 
         document.getElementById('submit').addEventListener('click', function() {
@@ -170,11 +167,17 @@
               summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
               summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
             }
+			
+			document.getElementById("directions-panel").className =
+			document.getElementById("directions-panel").className.replace(/\bhidden\b/,'');
+	
+	
           } else {
             window.alert('Directions request failed due to ' + status);
           }
         });
       }
+	  
     </script>
     <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBM8LtJcXEjCABtsGyigU0dCN4agXmvmag&callback=initMap">
